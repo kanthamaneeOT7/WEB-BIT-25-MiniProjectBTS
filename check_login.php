9036afa7b230ee66d0ebb5ef34e57cca3fde2175
kanthamaneeOT7 <melancholicdearly7@gmail.com>
2026-09-15 14:33:53 +0700

    finalpt2

--- check_login.php
+++ check_login.php
@@ -1,6 +1,6 @@
 <?php
 
-$con=mysqli_connect("localhost","root","","BTS_MiniPJ_login");
+$con=mysqli_connect("localhost","root","","BTS_MiniPJ");
 
 $username= $_POST['username'];
 $password= $_POST['password'];
@@ -16,7 +16,7 @@ $result= mysqli_query($con,$q);
 // login success
 if(mysqli_num_rows($result)> 0 ){
     $user=mysqli_fetch_assoc($result);
-    $_SESSION["fname"]=$user["fname"];
+    $_SESSION["fname"]=$user["faname"]; 
     header("location:index.php");
     exit;
 }else{
